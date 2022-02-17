947638c8a84bec94f527913c29e37bce8179ef5d
yuvrajSyadav <98956764+yuvrajSyadav@users.noreply.github.com>
2022-02-17 11:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def predict():
         else:
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=0
-        Year=2020-Year
+        Year=2022-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
